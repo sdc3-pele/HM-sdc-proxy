e754277b3e99c5a29f668c49859ac83bfe9de7cb
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-05 16:29:55 -0700

    stress tests for proxy

---
+++ k6Test.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  vus: 500,
+  duration: "60s",
+  rps: 500
+};
+
+export default function() {
+  //let res = http.post("http://127.0.0.1:3000/api/photos/?val=[123,456,789]");
+  //let res = http.get("http://127.0.0.1:3000/api/photos/11");
+  let res = http.get("http://127.0.0.1:3000/11");
+
+  check(res, {
+    "status was 200": (r) => r.status == 200,
+  });
+  sleep(1);
+};
